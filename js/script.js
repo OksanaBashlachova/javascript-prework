f87308580c133aaab12363a80f343caf28a3f5ec
Oksana Bashlachova <ookb2019@gmail.com>
2023-01-08 15:05:34 +0200

    Add functions to code

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
+function getMoveName(argMoveId){
+  if(argMoveId == 1){
+    return 'kamień';
+  }
+  else if (argMoveId == 2){
+    return 'papier';
+  }
+  else if (argMoveId == 3){
+    return 'nożyce';
+  }
+
+printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+  return 'nieznany ruch';
+}
+
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-let computerMove = 'nieznany ruch';
-
+let computerMove = getMoveName(randomNumber);
+/*
 if(randomNumber == 1){
   computerMove = 'kamień';
 }
@@ -13,15 +28,15 @@ else if (randomNumber == 2){
 else if (randomNumber == 3){
     computerMove = 'nożyce';
 }
-
+*/
 printMessage('Mój ruch to: ' + computerMove);
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = 'nieznany ruch';
-
+let playerMove = getMoveName(playerInput);
+/*
 if(playerInput == '1'){
 playerMove = 'kamień';
 }
@@ -31,9 +46,9 @@ playerMove = 'papier';
 else if (playerInput == '3'){
 playerMove = 'nożyce';
 }
-
+*/
 printMessage('Twój ruch to: ' + playerMove);
-
+/*
 if( computerMove == 'kamień' && playerMove == 'papier'){
 printMessage('Ty wygrywasz!');
 }
@@ -51,4 +66,28 @@ printMessage('nieznany ruch!');
 }
 else {
 printMessage('Ty prograwasz!');
+}
+*/
+console.log('moves:', argComputerMove, argPlayerMove);
+
+function displayResult(argComputerMove, argPlayerMove){
+
+  if( argComputerMove == 'kamień' && argPlayerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+    }
+    else if( argComputerMove == 'papier' && argPlayerMove == 'nożyce'){
+    printMessage('Ty wygrywasz!');
+    }
+    else if( argComputerMove == 'nożyce' && argPlayerMove == 'kamień'){
+    printMessage('Ty wygrywasz!');
+    }
+    else if( argComputerMove == argPlayerMove){
+      printMessage('Remis!');
+      }
+    else if( argPlayerMove == 'nieznany ruch'){
+    printMessage('nieznany ruch!');
+    }
+    else {
+    printMessage('Ty prograwasz!');
+    }
 }
